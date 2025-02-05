cb267a85151d1ac73e5d336d1186cc2d9a6c8b88
Blank <wangfj11@foxmail.com>
2025-02-05 20:46:07 +0800

    [增加]1. 增加HTTPClient的扩展函数

---
+++ GameFrameX.Foundation.Http.Extension/HttpClientPostExtension.cs
@@ -0,0 +1,171 @@
+﻿using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace GameFrameX.Foundation.Http.Extension;
+
+/// <summary>
+/// HttpClient的POST请求扩展方法
+/// </summary>
+public static class HttpClientPostExtension
+{
+    /// <summary>
+    /// JSON序列化选项，不区分大小写，使用驼峰命名
+    /// </summary>
+    private static readonly JsonSerializerOptions Options = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true, PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+
+    /// <summary>
+    /// 发送POST请求，将JSON数据序列化后发送，并将响应内容读取为字符串
+    /// </summary>
+    /// <typeparam name="TValue">要发送的数据类型</typeparam>
+    /// <param name="httpClient">HttpClient实例</param>
+    /// <param name="url">请求URL</param>
+    /// <param name="data">要发送的数据</param>
+    /// <param name="cancellationToken">取消令牌</param>
+    /// <returns>响应内容的字符串形式</returns>
+    /// <exception cref="ArgumentNullException">当httpClient或url为null时抛出</exception>
+    public static async Task<string> PostJsonToStringAsync<TValue>(this HttpClient httpClient, string url, TValue data, CancellationToken cancellationToken = default)
+    {
+        ArgumentNullException.ThrowIfNull(httpClient, nameof(httpClient));
+        ArgumentNullException.ThrowIfNull(url, nameof(url));
+        var response = await httpClient.PostAsJsonAsync<TValue>(url, data, Options, cancellationToken);
+        return await response.Content.ReadAsStringAsync(cancellationToken);
+    }
+
+    /// <summary>
+    /// 发送POST请求，将JSON数据序列化后发送，并将响应内容读取为字符串
+    /// </summary>
+    /// <typeparam name="TValue">要发送的数据类型</typeparam>
+    /// <param name="httpClient">HttpClient实例</param>
+    /// <param name="url">请求URL</param>
+    /// <param name="data">要发送的数据</param>
+    /// <param name="headers">请求头字典</param>
+    /// <param name="timeout">超时时间(秒)</param>
+    /// <param name="cancellationToken">取消令牌</param>
+    /// <returns>响应内容的字符串形式</returns>
+    /// <exception cref="ArgumentNullException">当httpClient或url为null时抛出</exception>
+    public static async Task<string> PostJsonToStringAsync<TValue>(this HttpClient httpClient, string url, TValue data, IDictionary<string, string> headers = null, int timeout = 10, CancellationToken cancellationToken = default)
+    {
+        ArgumentNullException.ThrowIfNull(httpClient, nameof(httpClient));
+        ArgumentNullException.ThrowIfNull(url, nameof(url));
+        httpClient.Timeout = TimeSpan.FromSeconds(timeout);
+        if (headers is { Count: > 0 })
+        {
+            foreach (var header in headers)
+            {
+                if (httpClient.DefaultRequestHeaders.Contains(header.Key))
+                {
+                    httpClient.DefaultRequestHeaders.Remove(header.Key);
+                }
+
+                httpClient.DefaultRequestHeaders.Add(header.Key, header.Value);
+            }
+        }
+
+        var response = await httpClient.PostAsJsonAsync<TValue>(url, data, Options, cancellationToken);
+        return await response.Content.ReadAsStringAsync(cancellationToken);
+    }
+
+    /// <summary>
+    /// 发送POST请求，将JSON数据序列化后发送，并将响应内容读取为字节数组
+    /// </summary>
+    /// <typeparam name="TValue">要发送的数据类型</typeparam>
+    /// <param name="httpClient">HttpClient实例</param>
+    /// <param name="url">请求URL</param>
+    /// <param name="data">要发送的数据</param>
+    /// <param name="cancellationToken">取消令牌</param>
+    /// <returns>响应内容的字节数组形式</returns>
+    /// <exception cref="ArgumentNullException">当httpClient或url为null时抛出</exception>
+    public static async Task<byte[]> PostJsonToByteArrayAsync<TValue>(this HttpClient httpClient, string url, TValue data, CancellationToken cancellationToken = default)
+    {
+        ArgumentNullException.ThrowIfNull(httpClient, nameof(httpClient));
+        ArgumentNullException.ThrowIfNull(url, nameof(url));
+        var response = await httpClient.PostAsJsonAsync<TValue>(url, data, Options, cancellationToken);
+        return await response.Content.ReadAsByteArrayAsync(cancellationToken);
+    }
+
+    /// <summary>
+    /// 发送POST请求，将JSON数据序列化后发送，并将响应内容读取为字节数组
+    /// </summary>
+    /// <typeparam name="TValue">要发送的数据类型</typeparam>
+    /// <param name="httpClient">HttpClient实例</param>
+    /// <param name="url">请求URL</param>
+    /// <param name="data">要发送的数据</param>
+    /// <param name="headers">请求头字典</param>
+    /// <param name="timeout">超时时间(秒)</param>
+    /// <param name="cancellationToken">取消令牌</param>
+    /// <returns>响应内容的字节数组形式</returns>
+    /// <exception cref="ArgumentNullException">当httpClient或url为null时抛出</exception>
+    public static async Task<byte[]> PostJsonToByteArrayAsync<TValue>(this HttpClient httpClient, string url, TValue data, IDictionary<string, string> headers = null, int timeout = 10, CancellationToken cancellationToken = default)
+    {
+        ArgumentNullException.ThrowIfNull(httpClient, nameof(httpClient));
+        ArgumentNullException.ThrowIfNull(url, nameof(url));
+        httpClient.Timeout = TimeSpan.FromSeconds(timeout);
+        if (headers is { Count: > 0 })
+        {
+            foreach (var header in headers)
+            {
+                if (httpClient.DefaultRequestHeaders.Contains(header.Key))
+                {
+                    httpClient.DefaultRequestHeaders.Remove(header.Key);
+                }
+
+                httpClient.DefaultRequestHeaders.Add(header.Key, header.Value);
+            }
+        }
+
+        var response = await httpClient.PostAsJsonAsync<TValue>(url, data, Options, cancellationToken);
+        return await response.Content.ReadAsByteArrayAsync(cancellationToken);
+    }
+
+    /// <summary>
+    /// 发送POST请求，将JSON数据序列化后发送，并将响应内容读取为流
+    /// </summary>
+    /// <typeparam name="TValue">要发送的数据类型</typeparam>
+    /// <param name="httpClient">HttpClient实例</param>
+    /// <param name="url">请求URL</param>
+    /// <param name="data">要发送的数据</param>
+    /// <param name="cancellationToken">取消令牌</param>
+    /// <returns>响应内容的流形式</returns>
+    /// <exception cref="ArgumentNullException">当httpClient或url为null时抛出</exception>
+    public static async Task<Stream> PostJsonToStreamAsync<TValue>(this HttpClient httpClient, string url, TValue data, CancellationToken cancellationToken = default)
+    {
+        ArgumentNullException.ThrowIfNull(httpClient, nameof(httpClient));
+        ArgumentNullException.ThrowIfNull(url, nameof(url));
+        var response = await httpClient.PostAsJsonAsync<TValue>(url, data, Options, cancellationToken);
+        return await response.Content.ReadAsStreamAsync(cancellationToken);
+    }
+
+    /// <summary>
+    /// 发送POST请求，将JSON数据序列化后发送，并将响应内容读取为流
+    /// </summary>
+    /// <typeparam name="TValue">要发送的数据类型</typeparam>
+    /// <param name="httpClient">HttpClient实例</param>
+    /// <param name="url">请求URL</param>
+    /// <param name="data">要发送的数据</param>
+    /// <param name="headers">请求头字典</param>
+    /// <param name="timeout">超时时间(秒)</param>
+    /// <param name="cancellationToken">取消令牌</param>
+    /// <returns>响应内容的流形式</returns>
+    /// <exception cref="ArgumentNullException">当httpClient或url为null时抛出</exception>
+    public static async Task<Stream> PostJsonToStreamAsync<TValue>(this HttpClient httpClient, string url, TValue data, IDictionary<string, string> headers = null, int timeout = 10, CancellationToken cancellationToken = default)
+    {
+        ArgumentNullException.ThrowIfNull(httpClient, nameof(httpClient));
+        ArgumentNullException.ThrowIfNull(url, nameof(url));
+        httpClient.Timeout = TimeSpan.FromSeconds(timeout);
+        if (headers is { Count: > 0 })
+        {
+            foreach (var header in headers)
+            {
+                if (httpClient.DefaultRequestHeaders.Contains(header.Key))
+                {
+                    httpClient.DefaultRequestHeaders.Remove(header.Key);
+                }
+
+                httpClient.DefaultRequestHeaders.Add(header.Key, header.Value);
+            }
+        }
+
+        var response = await httpClient.PostAsJsonAsync<TValue>(url, data, Options, cancellationToken);
+        return await response.Content.ReadAsStreamAsync(cancellationToken);
+    }
+}
