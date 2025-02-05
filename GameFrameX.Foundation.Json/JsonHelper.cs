cbd49adbc9eebbf3c770f8e16d099846159a188d
Blank <wangfj11@foxmail.com>
2025-02-05 21:04:43 +0800

    [增加]1. 增加JSON基础库

---
+++ GameFrameX.Foundation.Json/JsonHelper.cs
@@ -0,0 +1,375 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace GameFrameX.Foundation.Json;
+
+/// <summary>
+/// JSON序列化和反序列化的帮助类
+/// 提供了默认和格式化两种序列化配置选项
+/// 支持枚举字符串转换、忽略null值、忽略循环引用等功能
+/// </summary>
+public static class JsonHelper
+{
+    /// <summary>
+    /// 默认序列化配置
+    /// 包含以下特性:
+    /// <list type="bullet">
+    /// <item><description>忽略null值属性</description></item>
+    /// <item><description>忽略循环引用</description></item>
+    /// <item><description>属性名称大小写不敏感</description></item>
+    /// <item><description>枚举值序列化为字符串</description></item>
+    /// </list>
+    /// </summary>
+    public readonly static JsonSerializerOptions DefaultOptions = new JsonSerializerOptions
+    {
+        // 忽略 null 值
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        // 忽略循环引用
+        ReferenceHandler = ReferenceHandler.IgnoreCycles,
+        // 大小写判断
+        PropertyNameCaseInsensitive = true,
+        // 添加自定义转换器，类似于 StringEnumConverter
+        Converters =
+        {
+            new JsonStringEnumConverter() // 处理枚举为字符串
+        },
+    };
+
+    /// <summary>
+    /// 格式化序列化配置
+    /// 在默认配置基础上增加了JSON格式化功能
+    /// 包含以下特性:
+    /// <list type="bullet">
+    /// <item><description>忽略null值属性</description></item>
+    /// <item><description>忽略循环引用</description></item>
+    /// <item><description>属性名称大小写不敏感</description></item>
+    /// <item><description>枚举值序列化为字符串</description></item>
+    /// <item><description>输出格式化的JSON文本(包含缩进和换行)</description></item>
+    /// </list>
+    /// </summary>
+    public readonly static JsonSerializerOptions FormatOptions = new JsonSerializerOptions
+    {
+        // 忽略 null 值
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        // 忽略循环引用
+        ReferenceHandler = ReferenceHandler.IgnoreCycles,
+        // 大小写判断
+        PropertyNameCaseInsensitive = true,
+        // 格式化 JSON
+        WriteIndented = true,
+        // 添加自定义转换器，类似于 StringEnumConverter
+        Converters =
+        {
+            new JsonStringEnumConverter() // 处理枚举为字符串
+        },
+    };
+
+    /// <summary>
+    /// 将对象序列化为JSON字符串
+    /// 使用默认序列化配置(DefaultOptions)
+    /// </summary>
+    /// <param name="obj">需要序列化的对象</param>
+    /// <returns>序列化后的JSON字符串</returns>
+    /// <exception cref="ArgumentNullException">当obj为null时抛出</exception>
+    public static string Serialize(object obj)
+    {
+        ArgumentNullException.ThrowIfNull(obj, nameof(obj));
+        var json = JsonSerializer.Serialize(obj, DefaultOptions);
+        return json;
+    }
+
+    /// <summary>
+    /// 将对象序列化为JSON字符串
+    /// 使用自定义序列化配置
+    /// </summary>
+    /// <param name="obj">需要序列化的对象</param>
+    /// <param name="options">自定义序列化配置</param>
+    /// <returns>序列化后的JSON字符串</returns>
+    /// <exception cref="ArgumentNullException">当obj或options为null时抛出</exception>
+    public static string Serialize(object obj, JsonSerializerOptions options)
+    {
+        ArgumentNullException.ThrowIfNull(obj, nameof(obj));
+        ArgumentNullException.ThrowIfNull(options, nameof(options));
+        var json = JsonSerializer.Serialize(obj, options);
+        return json;
+    }
+
+    /// <summary>
+    /// 将对象序列化为格式化的JSON字符串
+    /// 使用格式化序列化配置(FormatOptions)，生成的JSON包含适当的缩进和换行
+    /// </summary>
+    /// <param name="obj">需要序列化的对象</param>
+    /// <returns>格式化后的JSON字符串</returns>
+    /// <exception cref="ArgumentNullException">当obj为null时抛出</exception>
+    public static string SerializeFormat(object obj)
+    {
+        ArgumentNullException.ThrowIfNull(obj, nameof(obj));
+        var json = JsonSerializer.Serialize(obj, FormatOptions);
+        return json;
+    }
+
+    /// <summary>
+    /// 将对象序列化为UTF8编码的字节数组
+    /// 使用默认序列化配置(DefaultOptions)
+    /// </summary>
+    /// <param name="obj">需要序列化的对象</param>
+    /// <returns>序列化后的UTF8字节数组</returns>
+    /// <exception cref="ArgumentNullException">当obj为null时抛出</exception>
+    public static byte[] SerializeToUtf8Bytes(object obj)
+    {
+        ArgumentNullException.ThrowIfNull(obj, nameof(obj));
+        return JsonSerializer.SerializeToUtf8Bytes(obj, DefaultOptions);
+    }
+
+    /// <summary>
+    /// 将对象序列化为UTF8编码的字节数组
+    /// 使用自定义序列化配置
+    /// </summary>
+    /// <param name="obj">需要序列化的对象</param>
+    /// <param name="options">自定义序列化配置</param>
+    /// <returns>序列化后的UTF8字节数组</returns>
+    /// <exception cref="ArgumentNullException">当obj或options为null时抛出</exception>
+    public static byte[] SerializeToUtf8Bytes(object obj, JsonSerializerOptions options)
+    {
+        ArgumentNullException.ThrowIfNull(obj, nameof(obj));
+        ArgumentNullException.ThrowIfNull(options, nameof(options));
+        return JsonSerializer.SerializeToUtf8Bytes(obj, options);
+    }
+
+    /// <summary>
+    /// 将对象序列化为格式化的UTF8编码字节数组
+    /// 使用格式化序列化配置(FormatOptions)
+    /// </summary>
+    /// <param name="obj">需要序列化的对象</param>
+    /// <returns>序列化后的UTF8字节数组</returns>
+    /// <exception cref="ArgumentNullException">当obj为null时抛出</exception>
+    public static byte[] SerializeToUtf8BytesFormat(object obj)
+    {
+        ArgumentNullException.ThrowIfNull(obj, nameof(obj));
+        return JsonSerializer.SerializeToUtf8Bytes(obj, FormatOptions);
+    }
+
+    /// <summary>
+    /// 将JSON字符串反序列化为指定类型的对象
+    /// 使用默认序列化配置(DefaultOptions)
+    /// </summary>
+    /// <param name="json">需要反序列化的JSON字符串</param>
+    /// <typeparam name="T">目标类型，必须是引用类型且有无参构造函数</typeparam>
+    /// <returns>反序列化后的对象实例</returns>
+    /// <exception cref="ArgumentNullException">当json为null时抛出</exception>
+    /// <exception cref="ArgumentException">当json为空字符串或仅包含空白字符时抛出</exception>
+    public static T Deserialize<T>(string json) where T : class, new()
+    {
+        ArgumentNullException.ThrowIfNull(json, nameof(json));
+        ArgumentException.ThrowIfNullOrEmpty(json, nameof(json));
+        ArgumentException.ThrowIfNullOrWhiteSpace(json, nameof(json));
+        return JsonSerializer.Deserialize<T>(json, DefaultOptions);
+    }
+
+    /// <summary>
+    /// 将JSON字符串反序列化为指定类型的对象
+    /// 使用自定义序列化配置
+    /// </summary>
+    /// <param name="json">需要反序列化的JSON字符串</param>
+    /// <param name="options">自定义序列化配置</param>
+    /// <typeparam name="T">目标类型，必须是引用类型且有无参构造函数</typeparam>
+    /// <returns>反序列化后的对象实例</returns>
+    /// <exception cref="ArgumentNullException">当json或options为null时抛出</exception>
+    /// <exception cref="ArgumentException">当json为空字符串或仅包含空白字符时抛出</exception>
+    public static T Deserialize<T>(string json, JsonSerializerOptions options) where T : class, new()
+    {
+        ArgumentNullException.ThrowIfNull(json, nameof(json));
+        ArgumentException.ThrowIfNullOrEmpty(json, nameof(json));
+        ArgumentException.ThrowIfNullOrWhiteSpace(json, nameof(json));
+        ArgumentNullException.ThrowIfNull(options, nameof(options));
+        return JsonSerializer.Deserialize<T>(json, options);
+    }
+
+    /// <summary>
+    /// 将JSON字符串反序列化为指定Type类型的对象
+    /// 使用默认序列化配置(DefaultOptions)
+    /// </summary>
+    /// <param name="json">需要反序列化的JSON字符串</param>
+    /// <param name="type">目标类型的Type对象</param>
+    /// <returns>反序列化后的对象实例</returns>
+    /// <exception cref="ArgumentNullException">当json或type为null时抛出</exception>
+    /// <exception cref="ArgumentException">当json为空字符串或仅包含空白字符时抛出</exception>
+    public static object Deserialize(string json, Type type)
+    {
+        ArgumentNullException.ThrowIfNull(json, nameof(json));
+        ArgumentException.ThrowIfNullOrEmpty(json, nameof(json));
+        ArgumentException.ThrowIfNullOrWhiteSpace(json, nameof(json));
+        ArgumentNullException.ThrowIfNull(type, nameof(type));
+        return JsonSerializer.Deserialize(json, type, DefaultOptions);
+    }
+
+    /// <summary>
+    /// 将JSON字符串反序列化为指定Type类型的对象
+    /// 使用自定义序列化配置
+    /// </summary>
+    /// <param name="json">需要反序列化的JSON字符串</param>
+    /// <param name="type">目标类型的Type对象</param>
+    /// <param name="options">自定义序列化配置</param>
+    /// <returns>反序列化后的对象实例</returns>
+    /// <exception cref="ArgumentNullException">当json、type或options为null时抛出</exception>
+    /// <exception cref="ArgumentException">当json为空字符串或仅包含空白字符时抛出</exception>
+    public static object Deserialize(string json, Type type, JsonSerializerOptions options)
+    {
+        ArgumentNullException.ThrowIfNull(json, nameof(json));
+        ArgumentException.ThrowIfNullOrEmpty(json, nameof(json));
+        ArgumentException.ThrowIfNullOrWhiteSpace(json, nameof(json));
+        ArgumentNullException.ThrowIfNull(type, nameof(type));
+        ArgumentNullException.ThrowIfNull(options, nameof(options));
+        return JsonSerializer.Deserialize(json, type, options);
+    }
+
+    /// <summary>
+    /// 从UTF8编码的字节数组反序列化为指定类型的对象
+    /// 使用默认序列化配置(DefaultOptions)
+    /// </summary>
+    /// <param name="utf8Bytes">UTF8编码的JSON字节数组</param>
+    /// <typeparam name="T">目标类型，必须是引用类型且有无参构造函数</typeparam>
+    /// <returns>反序列化后的对象实例</returns>
+    /// <exception cref="ArgumentNullException">当utf8Bytes为null时抛出</exception>
+    public static T DeserializeFromUtf8Bytes<T>(byte[] utf8Bytes) where T : class, new()
+    {
+        ArgumentNullException.ThrowIfNull(utf8Bytes, nameof(utf8Bytes));
+        return JsonSerializer.Deserialize<T>(utf8Bytes, DefaultOptions);
+    }
+
+    /// <summary>
+    /// 从UTF8编码的字节数组反序列化为指定类型的对象
+    /// 使用自定义序列化配置
+    /// </summary>
+    /// <param name="utf8Bytes">UTF8编码的JSON字节数组</param>
+    /// <param name="options">自定义序列化配置</param>
+    /// <typeparam name="T">目标类型，必须是引用类型且有无参构造函数</typeparam>
+    /// <returns>反序列化后的对象实例</returns>
+    /// <exception cref="ArgumentNullException">当utf8Bytes或options为null时抛出</exception>
+    public static T DeserializeFromUtf8Bytes<T>(byte[] utf8Bytes, JsonSerializerOptions options) where T : class, new()
+    {
+        ArgumentNullException.ThrowIfNull(utf8Bytes, nameof(utf8Bytes));
+        ArgumentNullException.ThrowIfNull(options, nameof(options));
+        return JsonSerializer.Deserialize<T>(utf8Bytes, options);
+    }
+
+    /// <summary>
+    /// 从UTF8编码的字节数组反序列化为指定Type类型的对象
+    /// 使用默认序列化配置(DefaultOptions)
+    /// </summary>
+    /// <param name="utf8Bytes">UTF8编码的JSON字节数组</param>
+    /// <param name="type">目标类型的Type对象</param>
+    /// <returns>反序列化后的对象实例</returns>
+    /// <exception cref="ArgumentNullException">当utf8Bytes或type为null时抛出</exception>
+    public static object DeserializeFromUtf8Bytes(byte[] utf8Bytes, Type type)
+    {
+        ArgumentNullException.ThrowIfNull(utf8Bytes, nameof(utf8Bytes));
+        ArgumentNullException.ThrowIfNull(type, nameof(type));
+        return JsonSerializer.Deserialize(utf8Bytes, type, DefaultOptions);
+    }
+
+    /// <summary>
+    /// 从UTF8编码的字节数组反序列化为指定Type类型的对象
+    /// 使用自定义序列化配置
+    /// </summary>
+    /// <param name="utf8Bytes">UTF8编码的JSON字节数组</param>
+    /// <param name="type">目标类型的Type对象</param>
+    /// <param name="options">自定义序列化配置</param>
+    /// <returns>反序列化后的对象实例</returns>
+    /// <exception cref="ArgumentNullException">当utf8Bytes、type或options为null时抛出</exception>
+    public static object DeserializeFromUtf8Bytes(byte[] utf8Bytes, Type type, JsonSerializerOptions options)
+    {
+        ArgumentNullException.ThrowIfNull(utf8Bytes, nameof(utf8Bytes));
+        ArgumentNullException.ThrowIfNull(type, nameof(type));
+        ArgumentNullException.ThrowIfNull(options, nameof(options));
+        return JsonSerializer.Deserialize(utf8Bytes, type, options);
+    }
+
+    /// <summary>
+    /// 尝试将JSON字符串反序列化为指定类型的对象
+    /// 使用默认序列化配置(DefaultOptions)
+    /// </summary>
+    /// <param name="json">需要反序列化的JSON字符串</param>
+    /// <param name="result">反序列化成功时的结果对象</param>
+    /// <typeparam name="T">目标类型，必须是引用类型且有无参构造函数</typeparam>
+    /// <returns>反序列化是否成功</returns>
+    public static bool TryDeserialize<T>(string json, out T result) where T : class, new()
+    {
+        try
+        {
+            result = Deserialize<T>(json);
+            return true;
+        }
+        catch
+        {
+            result = default;
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// 尝试将JSON字符串反序列化为指定类型的对象
+    /// 使用自定义序列化配置
+    /// </summary>
+    /// <param name="json">需要反序列化的JSON字符串</param>
+    /// <param name="result">反序列化成功时的结果对象</param>
+    /// <param name="options">自定义序列化配置</param>
+    /// <typeparam name="T">目标类型，必须是引用类型且有无参构造函数</typeparam>
+    /// <returns>反序列化是否成功</returns>
+    public static bool TryDeserialize<T>(string json, out T result, JsonSerializerOptions options) where T : class, new()
+    {
+        try
+        {
+            result = Deserialize<T>(json, options);
+            return true;
+        }
+        catch
+        {
+            result = default;
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// 尝试将对象序列化为JSON字符串
+    /// 使用默认序列化配置(DefaultOptions)
+    /// </summary>
+    /// <param name="obj">需要序列化的对象</param>
+    /// <param name="result">序列化成功时的JSON字符串</param>
+    /// <returns>序列化是否成功</returns>
+    public static bool TrySerialize(object obj, out string result)
+    {
+        try
+        {
+            result = Serialize(obj);
+            return true;
+        }
+        catch
+        {
+            result = default;
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// 尝试将对象序列化为JSON字符串
+    /// 使用自定义序列化配置
+    /// </summary>
+    /// <param name="obj">需要序列化的对象</param>
+    /// <param name="result">序列化成功时的JSON字符串</param>
+    /// <param name="options">自定义序列化配置</param>
+    /// <returns>序列化是否成功</returns>
+    public static bool TrySerialize(object obj, out string result, JsonSerializerOptions options)
+    {
+        try
+        {
+            result = Serialize(obj, options);
+            return true;
+        }
+        catch
+        {
+            result = default;
+            return false;
+        }
+    }
+}
