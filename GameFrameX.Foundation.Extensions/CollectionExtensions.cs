9a991885caa1a87c548085dfee6d55f81a66de6e
Blank <wangfj11@foxmail.com>
2026-03-19 20:12:15 +0800

    docs: 为集合扩展方法添加英文文档注释
    为 CollectionExtensions 类及其所有方法添加英文文档注释，提高代码的可读性和国际化支持。

--- GameFrameX.Foundation.Extensions/CollectionExtensions.cs
+++ GameFrameX.Foundation.Extensions/CollectionExtensions.cs
@@ -37,8 +37,11 @@ using GameFrameX.Foundation.Extensions.Localization;
 namespace GameFrameX.Foundation.Extensions;
 
 /// <summary>
-/// 提供集合类型的扩展方法
+/// 提供集合类型的扩展方法。
 /// </summary>
+/// <remarks>
+/// Provides extension methods for collection types.
+/// </remarks>
 public static class CollectionExtensions
 {
     #region ICollectionExtensions
@@ -46,9 +49,12 @@ public static class CollectionExtensions
     /// <summary>
     /// 检查集合是否为 null 或空。
     /// </summary>
-    /// <typeparam name="T">集合元素的类型。</typeparam>
-    /// <param name="self">要检查的集合。</param>
-    /// <returns>如果集合为 null 或空，则为 true；否则为 false。</returns>
+    /// <remarks>
+    /// Checks whether the collection is null or empty.
+    /// </remarks>
+    /// <typeparam name="T">集合元素的类型 / The type of elements in the collection.</typeparam>
+    /// <param name="self">要检查的集合 / The collection to check.</param>
+    /// <returns>如果集合为 null 或空，则为 true；否则为 false / true if the collection is null or empty; otherwise, false.</returns>
     public static bool IsNullOrEmpty<T>(this ICollection<T> self)
     {
         return self is not { Count: > 0 };
@@ -59,11 +65,14 @@ public static class CollectionExtensions
     /// <summary>
     /// 将一个可枚举集合的元素添加到哈希集合中。
     /// </summary>
-    /// <typeparam name="T">哈希集合元素的类型。</typeparam>
-    /// <param name="hashSet">要添加元素的哈希集合，不能为 null。</param>
-    /// <param name="enumerable">要添加的元素的可枚举集合，不能为 null。</param>
-    /// <exception cref="ArgumentNullException">当 <paramref name="hashSet"/> 或 <paramref name="enumerable"/> 为 null 时抛出</exception>
-    /// <remarks>此方法会将指定集合中的所有元素添加到目标哈希集合中。如果元素已存在，则会被忽略。</remarks>
+    /// <remarks>
+    /// Adds the elements of the specified collection to the <see cref="HashSet{T}"/>.
+    /// If an element already exists in the set, it will be ignored.
+    /// </remarks>
+    /// <typeparam name="T">哈希集合元素的类型 / The type of elements in the hash set.</typeparam>
+    /// <param name="hashSet">要添加元素的哈希集合，不能为 null / The hash set to add elements to, cannot be null.</param>
+    /// <param name="enumerable">要添加的元素的可枚举集合，不能为 null / The collection whose elements should be added, cannot be null.</param>
+    /// <exception cref="ArgumentNullException">当 <paramref name="hashSet"/> 或 <paramref name="enumerable"/> 为 null 时抛出 / Thrown when <paramref name="hashSet"/> or <paramref name="enumerable"/> is null.</exception>
     public static void AddRangeValues<T>(this HashSet<T> hashSet, IEnumerable<T> enumerable)
     {
         ArgumentNullException.ThrowIfNull(hashSet, nameof(hashSet));
@@ -80,17 +89,16 @@ public static class CollectionExtensions
     /// <summary>
     /// 合并字典中的键值对。如果字典中已存在指定的键，则使用指定的函数对原有值和新值进行合并；否则直接添加键值对。
     /// </summary>
-    /// <typeparam name="TKey">键的类型。</typeparam>
-    /// <typeparam name="TValue">值的类型。</typeparam>
-    /// <param name="self">要合并的字典，不能为 null。</param>
-    /// <param name="key">要添加或合并的键，不能为 null（如果 TKey 是引用类型）。</param>
-    /// <param name="v">要添加或合并的值。</param>
-    /// <param name="func">用于合并值的函数，接收两个参数：现有值和新值，返回合并后的结果，不能为 null。</param>
-    /// <exception cref="ArgumentNullException">当 <paramref name="self"/>、<paramref name="key"/>（如果是引用类型）、<paramref name="v"/>（如果是引用类型）或 <paramref name="func"/> 为 null 时抛出</exception>
     /// <remarks>
-    /// 当键已存在时，将调用提供的合并函数来组合现有值和新值。
-    /// 当键不存在时，直接将新值添加到字典中。
+    /// Merges a key/value pair into the dictionary. If the key already exists, the existing value and new value are combined using the specified function; otherwise, the key/value pair is added directly.
     /// </remarks>
+    /// <typeparam name="TKey">键的类型 / The type of the keys in the dictionary.</typeparam>
+    /// <typeparam name="TValue">值的类型 / The type of the values in the dictionary.</typeparam>
+    /// <param name="self">要合并的字典，不能为 null / The dictionary to merge into, cannot be null.</param>
+    /// <param name="key">要添加或合并的键 / The key to add or merge.</param>
+    /// <param name="v">要添加或合并的值 / The value to add or merge.</param>
+    /// <param name="func">用于合并值的函数，接收两个参数：现有值和新值，返回合并后的结果，不能为 null / The function to use to merge values if the key exists, cannot be null.</param>
+    /// <exception cref="ArgumentNullException">当 <paramref name="self"/>、<paramref name="key"/>（如果是引用类型）、<paramref name="v"/>（如果是引用类型）或 <paramref name="func"/> 为 null 时抛出 / Thrown when <paramref name="self"/>, <paramref name="key"/> (if reference type), <paramref name="v"/> (if reference type), or <paramref name="func"/> is null.</exception>
     public static void Merge<TKey, TValue>(this Dictionary<TKey, TValue> self, TKey key, TValue v, Func<TValue, TValue, TValue> func)
     {
         ArgumentNullException.ThrowIfNull(self, nameof(self));
@@ -108,13 +116,16 @@ public static class CollectionExtensions
     /// <summary>
     /// 获取指定键的值，如果字典中不存在该键，则使用指定的函数获取值并添加到字典中。
     /// </summary>
-    /// <typeparam name="TKey">键的类型。</typeparam>
-    /// <typeparam name="TValue">值的类型。</typeparam>
-    /// <param name="self">要操作的字典，不能为 null。</param>
-    /// <param name="key">要获取值的键，不能为 null（如果 TKey 是引用类型）。</param>
-    /// <param name="valueGetter">用于获取值的函数，接收键作为参数并返回对应的值，不能为 null。</param>
-    /// <returns>指定键的值。如果键不存在，则返回新创建的值。</returns>
-    /// <exception cref="ArgumentNullException">当 <paramref name="self"/>、<paramref name="key"/>（如果是引用类型）或 <paramref name="valueGetter"/> 为 null 时抛出</exception>
+    /// <remarks>
+    /// Gets the value associated with the specified key. If the key does not exist, the value is created using the specified function and added to the dictionary.
+    /// </remarks>
+    /// <typeparam name="TKey">键的类型 / The type of the keys in the dictionary.</typeparam>
+    /// <typeparam name="TValue">值的类型 / The type of the values in the dictionary.</typeparam>
+    /// <param name="self">要操作的字典，不能为 null / The dictionary to operate on, cannot be null.</param>
+    /// <param name="key">要获取值的键 / The key whose value to get.</param>
+    /// <param name="valueGetter">用于获取值的函数，接收键作为参数并返回对应的值，不能为 null / The function to use to generate a value if the key is not found, cannot be null.</param>
+    /// <returns>指定键的值。如果键不存在，则返回新创建的值 / The value associated with the specified key. If the key does not exist, returns the newly created value.</returns>
+    /// <exception cref="ArgumentNullException">当 <paramref name="self"/>、<paramref name="key"/>（如果是引用类型）或 <paramref name="valueGetter"/> 为 null 时抛出 / Thrown when <paramref name="self"/>, <paramref name="key"/> (if reference type), or <paramref name="valueGetter"/> is null.</exception>
     public static TValue GetOrAddValue<TKey, TValue>(this Dictionary<TKey, TValue> self, TKey key, Func<TKey, TValue> valueGetter)
     {
         ArgumentNullException.ThrowIfNull(self, nameof(self));
@@ -133,12 +144,15 @@ public static class CollectionExtensions
     /// <summary>
     /// 获取指定键的值，如果字典中不存在该键，则使用无参构造函数创建一个新的值并添加到字典中。
     /// </summary>
-    /// <typeparam name="TKey">键的类型。</typeparam>
-    /// <typeparam name="TValue">值的类型，必须包含无参构造函数。</typeparam>
-    /// <param name="self">要操作的字典，不能为 null。</param>
-    /// <param name="key">要获取值的键，不能为 null（如果 TKey 是引用类型）。</param>
-    /// <returns>指定键的值。如果键不存在，则返回新创建的值。</returns>
-    /// <exception cref="ArgumentNullException">当 <paramref name="self"/> 或 <paramref name="key"/> 为 null 时抛出</exception>
+    /// <remarks>
+    /// Gets the value associated with the specified key. If the key does not exist, a new value is created using the parameterless constructor and added to the dictionary.
+    /// </remarks>
+    /// <typeparam name="TKey">键的类型 / The type of the keys in the dictionary.</typeparam>
+    /// <typeparam name="TValue">值的类型，必须包含无参构造函数 / The type of the values in the dictionary, must have a parameterless constructor.</typeparam>
+    /// <param name="self">要操作的字典，不能为 null / The dictionary to operate on, cannot be null.</param>
+    /// <param name="key">要获取值的键 / The key whose value to get.</param>
+    /// <returns>指定键的值。如果键不存在，则返回新创建的值 / The value associated with the specified key. If the key does not exist, returns the newly created value.</returns>
+    /// <exception cref="ArgumentNullException">当 <paramref name="self"/> 或 <paramref name="key"/> 为 null 时抛出 / Thrown when <paramref name="self"/> or <paramref name="key"/> is null.</exception>
     public static TValue GetOrAddValue<TKey, TValue>(this Dictionary<TKey, TValue> self, TKey key) where TValue : new()
     {
         return GetOrAddValue(self, key, _ => new TValue());
@@ -147,17 +161,17 @@ public static class CollectionExtensions
     /// <summary>
     /// 根据指定条件从字典中移除键值对。
     /// </summary>
-    /// <typeparam name="TKey">键的类型。</typeparam>
-    /// <typeparam name="TValue">值的类型。</typeparam>
-    /// <param name="self">要操作的字典，不能为 null。</param>
-    /// <param name="predict">判断是否移除键值对的条件，接收键和值作为参数，返回true表示需要移除，不能为 null。</param>
-    /// <returns>移除的键值对数量。</returns>
-    /// <exception cref="ArgumentNullException">当 <paramref name="self"/> 或 <paramref name="predict"/> 为 null 时抛出</exception>
     /// <remarks>
-    /// 此方法会遍历字典中的所有键值对，对每一对调用predict函数进行判断。
-    /// 如果predict返回true，则移除该键值对。
-    /// 为避免在遍历时修改集合导致的异常，此方法会先收集要移除的键，然后统一移除。
+    /// Removes all key/value pairs from the dictionary that match the specified condition.
+    /// This method iterates through all key/value pairs and removes those where the predicate returns true.
+    /// To avoid modifying the collection during iteration, keys to remove are collected first, then removed.
     /// </remarks>
+    /// <typeparam name="TKey">键的类型 / The type of the keys in the dictionary.</typeparam>
+    /// <typeparam name="TValue">值的类型 / The type of the values in the dictionary.</typeparam>
+    /// <param name="self">要操作的字典，不能为 null / The dictionary to operate on, cannot be null.</param>
+    /// <param name="predict">判断是否移除键值对的条件，接收键和值作为参数，返回true表示需要移除，不能为 null / The predicate function to determine which items to remove, cannot be null.</param>
+    /// <returns>移除的键值对数量 / The number of items removed.</returns>
+    /// <exception cref="ArgumentNullException">当 <paramref name="self"/> 或 <paramref name="predict"/> 为 null 时抛出 / Thrown when <paramref name="self"/> or <paramref name="predict"/> is null.</exception>
     public static int RemoveIf<TKey, TValue>(this Dictionary<TKey, TValue> self, Func<TKey, TValue, bool> predict)
     {
         ArgumentNullException.ThrowIfNull(self, nameof(self));
@@ -187,12 +201,15 @@ public static class CollectionExtensions
     /// <summary>
     /// 从列表中随机获取一个元素。
     /// </summary>
-    /// <typeparam name="T">列表元素的类型。</typeparam>
-    /// <param name="list">要随机的列表，不能为 null 且不能为空。</param>
-    /// <returns>随机选择的列表元素。</returns>
-    /// <exception cref="ArgumentNullException">当 <paramref name="list"/> 为 null 时抛出</exception>
-    /// <exception cref="ArgumentException">当 <paramref name="list"/> 为空时抛出</exception>
-    /// <remarks>使用System.Random.Shared来生成随机数，确保线程安全。</remarks>
+    /// <remarks>
+    /// Returns a random element from the list.
+    /// Uses <see cref="System.Random.Shared"/> to generate random numbers, ensuring thread safety.
+    /// </remarks>
+    /// <typeparam name="T">列表元素的类型 / The type of elements in the list.</typeparam>
+    /// <param name="list">要随机的列表，不能为 null 且不能为空 / The list to get a random element from, cannot be null or empty.</param>
+    /// <returns>随机选择的列表元素 / A random element from the list.</returns>
+    /// <exception cref="ArgumentNullException">当 <paramref name="list"/> 为 null 时抛出 / Thrown when <paramref name="list"/> is null.</exception>
+    /// <exception cref="ArgumentException">当 <paramref name="list"/> 为空时抛出 / Thrown when <paramref name="list"/> is empty.</exception>
     public static T RandomElement<T>(this List<T> list)
     {
         ArgumentNullException.ThrowIfNull(list, nameof(list));
@@ -209,12 +226,15 @@ public static class CollectionExtensions
     /// <summary>
     /// 从列表中随机选择一个元素。
     /// </summary>
-    /// <typeparam name="T">列表元素的类型。</typeparam>
-    /// <param name="list">要从中选择元素的列表，不能为 null 且不能为空。</param>
-    /// <param name="random">用于生成随机数的 Random 实例，不能为 null。</param>
-    /// <returns>从列表中随机选择的元素。</returns>
-    /// <exception cref="ArgumentNullException">当 <paramref name="list"/> 或 <paramref name="random"/> 为 null 时抛出</exception>
-    /// <exception cref="ArgumentException">当 <paramref name="list"/> 为空时抛出</exception>
+    /// <remarks>
+    /// Returns a random element from the list using the specified random number generator.
+    /// </remarks>
+    /// <typeparam name="T">列表元素的类型 / The type of elements in the list.</typeparam>
+    /// <param name="list">要从中选择元素的列表，不能为 null 且不能为空 / The list to get a random element from, cannot be null or empty.</param>
+    /// <param name="random">用于生成随机数的 Random 实例，不能为 null / The random number generator to use, cannot be null.</param>
+    /// <returns>从列表中随机选择的元素 / A random element from the list.</returns>
+    /// <exception cref="ArgumentNullException">当 <paramref name="list"/> 或 <paramref name="random"/> 为 null 时抛出 / Thrown when <paramref name="list"/> or <paramref name="random"/> is null.</exception>
+    /// <exception cref="ArgumentException">当 <paramref name="list"/> 为空时抛出 / Thrown when <paramref name="list"/> is empty.</exception>
     public static T RandomElement<T>(this List<T> list, Random random)
     {
         ArgumentNullException.ThrowIfNull(list, nameof(list));
@@ -231,13 +251,13 @@ public static class CollectionExtensions
     /// <summary>
     /// 打乱列表中的元素顺序（洗牌）。
     /// </summary>
-    /// <typeparam name="T">列表元素的类型。</typeparam>
-    /// <param name="list">要打乱顺序的列表，不能为 null。</param>
-    /// <exception cref="ArgumentNullException">当 <paramref name="list"/> 为 null 时抛出</exception>
     /// <remarks>
-    /// 使用Fisher-Yates洗牌算法实现列表随机排序。
-    /// 该方法会直接修改原列表的顺序。
+    /// Shuffles the elements in the list using the Fisher-Yates algorithm.
+    /// This method modifies the original list directly.
     /// </remarks>
+    /// <typeparam name="T">列表元素的类型 / The type of elements in the list.</typeparam>
+    /// <param name="list">要打乱顺序的列表，不能为 null / The list to shuffle, cannot be null.</param>
+    /// <exception cref="ArgumentNullException">当 <paramref name="list"/> 为 null 时抛出 / Thrown when <paramref name="list"/> is null.</exception>
     public static void Shuffle<T>(this List<T> list)
     {
         ArgumentNullException.ThrowIfNull(list, nameof(list));
@@ -253,13 +273,14 @@ public static class CollectionExtensions
     /// <summary>
     /// 从列表中移除满足条件的所有元素。
     /// </summary>
-    /// <typeparam name="T">列表元素的类型。</typeparam>
-    /// <param name="list">要操作的列表，不能为 null。</param>
-    /// <param name="condition">用于判断元素是否满足移除条件的委托，返回true表示需要移除，不能为 null。</param>
-    /// <exception cref="ArgumentNullException">当 <paramref name="list"/> 或 <paramref name="condition"/> 为 null 时抛出</exception>
     /// <remarks>
-    /// 此方法使用 List{T}.RemoveAll 方法一次性移除所有满足条件的元素。
+    /// Removes all elements from the list that match the specified condition.
+    /// This method uses <see cref="List{T}.RemoveAll(Predicate{T})"/> to remove all matching elements at once.
     /// </remarks>
+    /// <typeparam name="T">列表元素的类型 / The type of elements in the list.</typeparam>
+    /// <param name="list">要操作的列表，不能为 null / The list to operate on, cannot be null.</param>
+    /// <param name="condition">用于判断元素是否满足移除条件的委托，返回true表示需要移除，不能为 null / The predicate function to determine which items to remove, cannot be null.</param>
+    /// <exception cref="ArgumentNullException">当 <paramref name="list"/> 或 <paramref name="condition"/> 为 null 时抛出 / Thrown when <paramref name="list"/> or <paramref name="condition"/> is null.</exception>
     public static void RemoveIf<T>(this List<T> list, Predicate<T> condition)
     {
         ArgumentNullException.ThrowIfNull(list, nameof(list));
