870075528e76eb2c9b37f2f92d9cd7f34b6d741c
Blank <wangfj11@foxmail.com>
2026-03-19 20:14:36 +0800

    docs: 为 DisposableConcurrentDictionary 添加英文注释
    添加 XML 文档注释的英文翻译，以提升代码的国际化和可读性。

--- GameFrameX.Foundation.Extensions/DisposableConcurrentDictionary.cs
+++ GameFrameX.Foundation.Extensions/DisposableConcurrentDictionary.cs
@@ -1,10 +1,13 @@
 namespace GameFrameX.Foundation.Extensions;
 
 /// <summary>
-/// 值可被Dispose的并发字典类型
+/// 值可被Dispose的并发字典类型。
 /// </summary>
-/// <typeparam name="TKey">键的类型。</typeparam>
-/// <typeparam name="TValue">值的类型，必须实现IDisposable接口。</typeparam>
+/// <remarks>
+/// A concurrent dictionary type whose values can be disposed.
+/// </remarks>
+/// <typeparam name="TKey">键的类型 / The type of the key.</typeparam>
+/// <typeparam name="TValue">值的类型，必须实现IDisposable接口 / The type of the value, must implement IDisposable interface.</typeparam>
 public class DisposableConcurrentDictionary<TKey, TValue> : NullableConcurrentDictionary<TKey, TValue>, IDisposable where TValue : IDisposable
 {
     private bool _isDisposed;
@@ -12,6 +15,9 @@ public class DisposableConcurrentDictionary<TKey, TValue> : NullableConcurrentDi
     /// <summary>
     /// 初始化一个新的 <see cref="DisposableConcurrentDictionary{TKey, TValue}" /> 实例。
     /// </summary>
+    /// <remarks>
+    /// Initializes a new instance of the <see cref="DisposableConcurrentDictionary{TKey, TValue}" /> class.
+    /// </remarks>
     public DisposableConcurrentDictionary()
     {
     }
@@ -19,7 +25,10 @@ public class DisposableConcurrentDictionary<TKey, TValue> : NullableConcurrentDi
     /// <summary>
     /// 使用指定的默认值初始化一个新的 <see cref="DisposableConcurrentDictionary{TKey, TValue}" /> 实例。
     /// </summary>
-    /// <param name="fallbackValue">当键不存在时返回的默认值。</param>
+    /// <remarks>
+    /// Initializes a new instance of the <see cref="DisposableConcurrentDictionary{TKey, TValue}" /> class with the specified default value.
+    /// </remarks>
+    /// <param name="fallbackValue">当键不存在时返回的默认值 / The default value to return when a key does not exist.</param>
     public DisposableConcurrentDictionary(TValue fallbackValue) : base(fallbackValue)
     {
     }
@@ -27,9 +36,12 @@ public class DisposableConcurrentDictionary<TKey, TValue> : NullableConcurrentDi
     /// <summary>
     /// 使用指定的并发级别和初始容量初始化一个新的 <see cref="DisposableConcurrentDictionary{TKey, TValue}" /> 实例。
     /// </summary>
-    /// <param name="concurrencyLevel">并发级别，即字典可以同时支持的线程数，必须大于0。</param>
-    /// <param name="capacity">字典的初始容量，必须大于等于0。</param>
-    /// <exception cref="ArgumentOutOfRangeException">当 concurrencyLevel 小于等于 0 或 capacity 小于 0 时抛出。</exception>
+    /// <remarks>
+    /// Initializes a new instance of the <see cref="DisposableConcurrentDictionary{TKey, TValue}" /> class with the specified concurrency level and initial capacity.
+    /// </remarks>
+    /// <param name="concurrencyLevel">并发级别，即字典可以同时支持的线程数，必须大于0 / The number of threads that can access the dictionary concurrently, must be greater than 0.</param>
+    /// <param name="capacity">字典的初始容量，必须大于等于0 / The initial number of elements that the dictionary can contain, must be greater than or equal to 0.</param>
+    /// <exception cref="ArgumentOutOfRangeException">当 <paramref name="concurrencyLevel"/> 小于等于 0 或 <paramref name="capacity"/> 小于 0 时抛出 / Thrown when <paramref name="concurrencyLevel"/> is less than or equal to 0 or <paramref name="capacity"/> is less than 0.</exception>
     public DisposableConcurrentDictionary(int concurrencyLevel, int capacity) : base(concurrencyLevel, capacity)
     {
         ArgumentOutOfRangeException.ThrowIfNegativeOrZero(concurrencyLevel, nameof(concurrencyLevel));
@@ -39,8 +51,11 @@ public class DisposableConcurrentDictionary<TKey, TValue> : NullableConcurrentDi
     /// <summary>
     /// 使用指定的比较器初始化一个新的 <see cref="DisposableConcurrentDictionary{TKey, TValue}" /> 实例。
     /// </summary>
-    /// <param name="comparer">用于比较键的比较器，不能为null。</param>
-    /// <exception cref="ArgumentNullException">当 comparer 为 null 时抛出。</exception>
+    /// <remarks>
+    /// Initializes a new instance of the <see cref="DisposableConcurrentDictionary{TKey, TValue}" /> class with the specified comparer.
+    /// </remarks>
+    /// <param name="comparer">用于比较键的比较器，不能为null / The equality comparison implementation to use when comparing keys, cannot be null.</param>
+    /// <exception cref="ArgumentNullException">当 <paramref name="comparer"/> 为 null 时抛出 / Thrown when <paramref name="comparer"/> is null.</exception>
     public DisposableConcurrentDictionary(IEqualityComparer<NullObject<TKey>> comparer) : base(comparer)
     {
         ArgumentNullException.ThrowIfNull(comparer, nameof(comparer));
@@ -49,6 +64,9 @@ public class DisposableConcurrentDictionary<TKey, TValue> : NullableConcurrentDi
     /// <summary>
     /// 释放资源。
     /// </summary>
+    /// <remarks>
+    /// Releases all resources used by this instance.
+    /// </remarks>
     public void Dispose()
     {
         if (_isDisposed)
@@ -64,6 +82,9 @@ public class DisposableConcurrentDictionary<TKey, TValue> : NullableConcurrentDi
     /// <summary>
     /// 终结器，确保未释放的资源在对象被垃圾回收时被释放。
     /// </summary>
+    /// <remarks>
+    /// Finalizer to ensure unmanaged resources are released when the object is garbage collected.
+    /// </remarks>
     ~DisposableConcurrentDictionary()
     {
         Dispose(false);
@@ -72,7 +93,10 @@ public class DisposableConcurrentDictionary<TKey, TValue> : NullableConcurrentDi
     /// <summary>
     /// 释放资源。
     /// </summary>
-    /// <param name="disposing">指示是否应释放托管资源。</param>
+    /// <remarks>
+    /// Releases the unmanaged resources used by the <see cref="DisposableConcurrentDictionary{TKey, TValue}"/> and optionally releases the managed resources.
+    /// </remarks>
+    /// <param name="disposing">指示是否应释放托管资源 / true to release both managed and unmanaged resources; false to release only unmanaged resources.</param>
     protected virtual void Dispose(bool disposing)
     {
         if (disposing)
